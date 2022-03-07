eaf03227a11833b5989f6199f96e490a08c09923
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-03-07 19:12:51 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+darkModeButton = document.getElementById("dark-mode-button")
+
+// handleButtonClick = function () {
+//     console.log("button clicked")
+//     // change the background colour and text
+//     body = document.body
+//     body.style.backgroundColor = "#091d1e"
+//     body.style.color = "#aaaaaa"
+
+//     // change the border of the title
+
+//     title = document.getElementById("title-section")
+//     title.style.border = "1px solid #aaaaaa"
+
+//     // change the border and headings on the posts
+
+//     posts = document.getElementsByClassName("post")
+//     for (post of posts){
+//         post.style.border = "1px solid #aaaaaa"
+//     }
+
+//     postHeadings = document.getElementsByTagName("h3")
+//     for (heading of postHeadings){
+//         heading.style.color = "#466876"
+//     }
+// }
+
+darkModeToggle = function(){
+    console.log("button clicked");
+    var element = document.body;
+    element.classList.toggle("dark-mode-toggle")
+}
+
+const btn = document.querySelectorAll(".btn").forEach(item => {
+    item.addEventListener("click", event => {
+        item.setAttribute("style", "background-color: pink");
+    });
+});
+
+//darkModeButton.addEventListener("click", handleButtonClick)
